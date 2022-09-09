a5ca20428f36f27635842db62c880cb4ec4d4152
developer <andy9c@gmail.com>
2022-09-09 09:09:09 +0530

    Sep 9 2020

---
+++ functions/src/index.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDJzgGKz0QPrBBHpOwEndp3Gsbcm_lZcmQ",
+  authDomain: "stpaulsrkl-erp.firebaseapp.com",
+  projectId: "stpaulsrkl-erp",
+  storageBucket: "stpaulsrkl-erp.appspot.com",
+  messagingSenderId: "761502801966",
+  appId: "1:761502801966:web:8fafbf1396f581f3783fbf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const defaultAnalytics = getAnalytics(app);
+const defaultFirestore = getFirestore(app);
+// Initialize Cloud Storage and get a reference to the service
+const defaultStorage = getStorage(app);
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -21,9 +21,12 @@ Future<void> main() async {
   await authenticationRepository.user.first;
 
   if (kIsWeb) {
+    // running on the web!
     await FirebaseAppCheck.instance.activate(
       webRecaptchaSiteKey: '6LfUzJgcAAAAABYmoyiNUOqaOcvMj8CXDyHTDr4p',
     );
+  } else {
+    // NOT running on the web! You can check for additional platforms here.
   }
 
   Bloc.observer = AppBlocObserver();
