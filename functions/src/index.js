c90f35dd53c38c01145f4eee7159be5040a0f58e
andy9c <andy9c@gmail.com>
2022-09-24 11:47:04 +0530

    Sep 24 2022

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { getPerformance } from "firebase/performance";
+import { initializeApp } from "@firebase/app";
+import { getAnalytics } from "@firebase/analytics";
+import { getFirestore } from "@firebase/firestore";
+import { getStorage } from "@firebase/storage";
+import { getPerformance } from "@firebase/performance";
 
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -11,12 +11,13 @@ import { getPerformance } from "firebase/performance";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDJzgGKz0QPrBBHpOwEndp3Gsbcm_lZcmQ",
-  authDomain: "stpaulsrkl-erp.firebaseapp.com",
-  projectId: "stpaulsrkl-erp",
-  storageBucket: "stpaulsrkl-erp.appspot.com",
-  messagingSenderId: "761502801966",
-  appId: "1:761502801966:web:8fafbf1396f581f3783fbf"
+  apiKey: "AIzaSyD71A_OGuYvtb7fTKPOr3FgWZ7Gsb7K3b8",
+  authDomain: "stpaulsrkl-admission.firebaseapp.com",
+  projectId: "stpaulsrkl-admission",
+  storageBucket: "stpaulsrkl-admission.appspot.com",
+  messagingSenderId: "167009655082",
+  appId: "1:167009655082:web:dac8d439eb7204f8876653",
+  measurementId: "G-4N2VB0G6L4"
 };
 
 // Initialize Firebase
